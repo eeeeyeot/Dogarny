becda1a87ed78b42c026d4d5c7d4e99b81236cb9
gksckdals234 <45381757+gksckdals234@users.noreply.github.com>
2019-04-20 19:58:40 +0900

    ??
    ㅁㄴㅇ

--- Dorgarny/Assets/Scripts/Stage/StageManager.cs
+++ Dorgarny/Assets/Scripts/Stage/StageManager.cs
@@ -14,7 +14,7 @@ public class StageManager : Singleton<StageManager>
         public int unLocked;
         public bool isInteractable;
         public int stars;
-        
+
     }
 
     public Transform chaTransform;
@@ -26,6 +26,8 @@ public class StageManager : Singleton<StageManager>
     public List<Stage> levelList;
 
     public GameObject button;
+
+    public float perspectiveZoomSpeed = 0.5f;
 
     void Start()
     {
@@ -46,9 +48,24 @@ public class StageManager : Singleton<StageManager>
 
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit hit;
+
+        //    if (Physics.Raycast(ray, out hit, 100.0f, 1 << LayerMask.NameToLayer("MoveAgent")))
+        //    {
+        //        //Debug.Log(hit.transform.gameObject.name);
+        //        Destination(hit.transform.name);
+        //        StageMove(hit.point);
+        //    }
+        //}
+
+        if (Input.touchCount == 1)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Touch touch = Input.GetTouch(0);
+            
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, 100.0f, 1 << LayerMask.NameToLayer("MoveAgent")))
@@ -57,6 +74,22 @@ public class StageManager : Singleton<StageManager>
                 Destination(hit.transform.name);
                 StageMove(hit.point);
             }
+        }
+        if(Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            float deltaMagnitudediff = prevTouchDeltaMag = touchDeltaMag;
+
+            Camera.main.fieldOfView += deltaMagnitudediff * perspectiveZoomSpeed;
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 7.0f, 20.0f);
         }
 
     }
--- Dorgarny/Assets/Scripts/Stage/Stage_Character.cs
+++ Dorgarny/Assets/Scripts/Stage/Stage_Character.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Stage_Character : MonoBehaviour
 {
+    private float offsetX = 10.0f;
+    private float offsetY = 9.0f;
+    private float offsetZ = 6.0f;
+
+    public void OnEnable()
+    {
+        Camera.main.transform.position = this.transform.position + new Vector3(offsetX, offsetY, offsetZ);
+        Camera.main.transform.rotation = Quaternion.Euler(new Vector3(45.0f, -90.0f, 0.0f));
+    }
     public void OnTriggerEnter(Collider other)
     {
         StageManager.Instance.CheckStage(other.name);
@@ -12,5 +21,10 @@ public class Stage_Character : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         StageManager.Instance.CheckStage(other.name);
+    }
+
+    public void Update()
+    {
+        //Camera.main.transform.position = this.transform.position + new Vector3(offsetX, offsetY, 0.0f);
     }
 }
