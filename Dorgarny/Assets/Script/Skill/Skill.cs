77eadc95edb3e68821ea434c70a36a782e3e80d6
eeeeyeot <39482454+eeeeyeot@users.noreply.github.com>
2019-04-20 19:58:32 +0900

    2019_04_20_중간 합침
    중간발표

---
+++ Dorgarny/Assets/Script/Skill/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "Skills/Skill")]
+public class Skill : ScriptableObject
+{
+	new public string name = "New Skill";
+	public Sprite icon = null;
+
+	public virtual void Use()
+	{
+		Debug.Log("Using Skill : " + name);
+	}
+}
