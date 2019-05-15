f904ed49c9099a0149b91d843ad54232311c8a22
ujon <dkcushow@gmail.com>
2019-05-15 18:00:13 +0900

    map
    from usb

---
+++ Dorgarny/Assets/Script/Character/Enemy/EnemyBasic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EnemyBasic : MonoBehaviour
+{
+    
+
+    [Header("Health")]
+    public Image healthBar;
+    public float startHealth = 100f;
+    public float health; //For Test: private => public
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = startHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = health / startHealth;
+        if (health <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Dorgarny/Assets/Script/Character/Enemy/EnemyHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+	public float startHealth = 100f;
+
+	float currentHealth;
+	bool isDead;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		currentHealth = startHealth;
+	}
+
+	void OnEnable()
+	{
+		currentHealth = startHealth;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void TakeDamage(int amount)
+	{
+
+	}
+
+	void Death()
+	{
+		isDead = true;
+	}
+}
