f26831d3880d605505e9d0441ed1367fb8abc95d
eeeeyeot <39482454+eeeeyeot@users.noreply.github.com>
2019-05-19 21:19:23 +0900

    tq

--- Dorgarny/Assets/SD RPG Character - Wizard/ReadMe (SD RPG Character).txt
+++
@@ -1,4 +0,0 @@
-[WasabiSnow] SD RPG Character
-
-Ver.1.0.1
-1. There are dummy for weapon.(DummyLHand)
--- Dorgarny/Assets/Script/Item/ConsumePotion.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Potion", menuName = "Inventory/Potion")]
-public class ConsumePotion : Item
-{
-    public int amount;
-}
